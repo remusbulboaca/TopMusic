fe30466dd1e6cec99a3f8ab5ed21e03da2bf9f67
remusbulboaca <47847935+remusbulboaca@users.noreply.github.com>
2020-12-21 11:50:58 +0200

    Primul test de db

--- server.c
+++ server.c
@@ -17,6 +17,7 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <pthread.h>
+#include <sqlite3.h>
 
 /* portul folosit */
 #define PORT 2908
@@ -130,6 +131,9 @@ void raspunde(void *arg)
         char data[50];
 	struct thData tdL;
 	tdL= *((struct thData*)arg);
+  //intializare db
+  sqlite3 *db;
+  sqlite3_stmt *stmt;
   /*Validam comanda primita de la client*/
   /* Este login/register/quit? */
   while (1) 
@@ -172,7 +176,42 @@ void raspunde(void *arg)
         }
   }
 
-  
+  sqlite3_open("TopMusic.db",&db);
+  if(db == NULL){
+    printf("Failed to open DB! \n");
+  }
+
+  sqlite3_prepare_v2(db,"select * from users",-1,&stmt,NULL);
+
+  printf("Results: \n");
+
+  while (sqlite3_step(stmt) != SQLITE_DONE) {
+		int i;
+		int num_cols = sqlite3_column_count(stmt);
+		
+		for (i = 0; i < num_cols; i++)
+		{
+			switch (sqlite3_column_type(stmt, i))
+			{
+			case (SQLITE3_TEXT):
+				printf("%s, ", sqlite3_column_text(stmt, i));
+				break;
+			case (SQLITE_INTEGER):
+				printf("%d, ", sqlite3_column_int(stmt, i));
+				break;
+			case (SQLITE_FLOAT):
+				printf("%g, ", sqlite3_column_double(stmt, i));
+				break;
+			default:
+				break;
+			}
+		}
+		printf("\n");
+
+	}
+
+  sqlite3_finalize(stmt);
+  sqlite3_close(db);
 
 
 		      
